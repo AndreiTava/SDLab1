e9860c93dac337c39cf687520f989a1981c51a23
Andrei Tava <tavaandrei@gmail.com>
2022-02-22 23:35:18 +0200

    reworked merge manually, "fixed" static aux problem

--- SDLab1/SDLab1.cpp
+++ SDLab1/SDLab1.cpp
@@ -24,6 +24,8 @@ vector<int> generate_test(default_random_engine& gen ,const int size, const int 
     return v;
 }
 
+
+
 bool is_sorted(vector<int>& v)
 {
     for (auto it = v.begin(); it != v.end() - 1; ++it)
@@ -79,22 +81,74 @@ void insertion_sort(vector<int>& v)
     }
 }
 
-void rec_merge(vector<int>::iterator left, vector<int>::iterator right)
+void rec_merge_short(vector<int>::iterator left, vector<int>::iterator right)
 {
     auto dist = distance(left,right);
     auto middle = left;
     advance(middle, dist / 2);
     if (dist > 2)
     {
-        rec_merge(left,middle);
-        rec_merge(middle,right);
+        rec_merge_short(left,middle);
+        rec_merge_short(middle,right);
     }
-    inplace_merge(left,middle, right);
+    inplace_merge(left,middle,right);
 }
-
+void rec_merge(vector<int>::iterator left, vector<int>::iterator right)
+{
+    auto dist = distance(left, right);
+    static vector<int> aux(dist);
+    if (dist > aux.size())
+        aux.resize(dist);
+    auto middle = left;
+    advance(middle, dist / 2);
+    if (dist > 2)
+    {
+        rec_merge(left, middle);
+        rec_merge(middle, right);
+    }
+    auto itlft = left, itrht = middle, itaux = aux.begin();
+    while (itlft != middle && itrht!= right)
+    {
+        if (itrht != right && *itlft <= *itrht)
+        {
+            *itaux = *itlft;
+            ++itaux;
+            ++itlft;
+        }
+        if (itlft != middle && *itrht <= *itlft)
+        {
+            *itaux = *itrht;
+            ++itaux;
+            ++itrht;
+        }
+    }
+    while (itlft != middle)
+    {
+        *itaux = *itlft;
+        ++itlft;
+        ++itaux;
+    }
+    while (itrht != right)
+    {
+        *itaux = *itrht;
+        ++itrht;
+        ++itaux;
+    }
+    for (auto itcpy = aux.begin(); itcpy != itaux; ++itcpy)
+    {
+        *left = *itcpy;
+        ++left;
+    }
+    if (itaux == aux.end())
+        aux.clear();
+}
 void merge_sort(vector<int>& v)
 {
     rec_merge(v.begin(), v.end());
+}
+void ftest()
+{
+    static vector<int> aux(5);
 }
 
 int main()
@@ -122,6 +176,7 @@ int main()
         auto beg = chrono::high_resolution_clock::now();
 
         merge_sort(v);
+        ftest();
 
         auto end = chrono::high_resolution_clock::now();
         auto time = chrono::duration_cast<chrono::microseconds>(end - beg);
